7917f05c60d2dc3da63b4f1d6e5b1092cdd61f8b
Shahzaib Ahmad <shahzaibahmediftikharahmed@gmial.com>
2025-09-17 00:04:12 +0500

    added FRONTEND_LINK in socket.js

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -4,45 +4,36 @@ import express from "express";
 
 const app = express();
 const server = http.createServer(app);
-
+const FRONTEND_LINK = process.env.FRONTEND_LINK;
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [FRONTEND_LINK],
   },
 });
-
 
 export function getReciverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-
 const userSocketMap = {};
-
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-  
   const userId = socket.handshake.query.userId;
-  
+
   if (userId) {
     userSocketMap[userId] = socket.id;
   }
 
-  
   io.emit("getOnlineUser", Object.keys(userSocketMap));
-  
 
-  
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
 
-    
     delete userSocketMap[userId];
 
-    
     io.emit("getOnlineUser", Object.keys(userSocketMap));
   });
 });
